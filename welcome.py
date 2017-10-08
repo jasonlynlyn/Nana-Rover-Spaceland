cb1012cae655ca66db6e650f4e0384c62994855d
Jason Lyn <jason.lyn.lol@gmail.com>
2017-10-08 22:32:57 +1100

    Add files via upload

--- welcome.py
+++ welcome.py
@@ -1,11 +1,38 @@
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+from kivy.uix.gridlayout import GridLayout
+from kivy.properties import NumericProperty
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.floatlayout import FloatLayout
 
-class MyApp(App):
+
+class WelcomeScreen(FloatLayout):
+
+	def __init__(self, **kwargs):
+		super(WelcomeScreen, self).__init__(**kwargs)
+		
+		self.add_widget(Label())
+		
+		self.add_widget(Button(text="Enter"))
+		
+		#self.BoxLayout(orientation="vertical")
+		#label=Label(text="Welcome")
+					
+
+
+p = Builder.load_file("myapp.kv")
+
+
+class MainApp(App):
 	
-	def build(App):
-		return Label(text="Welcome to Nana Rover Spaceland")
-		
-		
+	def build(self):
+		return p
+	
+
+
 if __name__=="__main__":
-	MyApp().run()
+	MainApp().run()
